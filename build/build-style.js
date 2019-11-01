83a4c8ee6723691d313a104639e737373e79ea6c
lushuaihua <zhuaiman@hotmail.com>
2019-11-01 15:41:49 +0800

    build逻辑优化

--- build/build-style.js
+++ build/build-style.js
@@ -4,6 +4,41 @@ const less = require('gulp-less');
 const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
 
+const fs = require('fs');
+ 
+function delDir(p) {
+    
+    // 读取文件夹中所有文件及文件夹
+    var list = fs.readdirSync(p);
+    list.forEach((v, i) => {
+      // 拼接路径
+        var url = p + '/' + v;
+      // 读取文件信息
+        var stats = fs.statSync(url);
+      // 判断是文件还是文件夹
+        if (stats.isFile()) {
+        // 当前为文件，则删除文件
+            fs.unlinkSync(url);
+        } else {
+        // 当前为文件夹，则递归调用自身
+            arguments.callee(url);
+        }
+    });
+    // 删除空文件夹
+    fs.rmdirSync(p);
+}
+  
+gulp.task('clean', function () {
+    if(!fs.existsSync('../dist')){
+        return;
+    }
+    delDir('../dist');
+    if(!fs.existsSync('../dist')){
+        return;
+    }
+    fs.rmdirSync('../dist');
+});
+
 // 编译less
 gulp.task('css', function () {
     gulp.src('../src/styles/index.less')
@@ -12,7 +47,7 @@ gulp.task('css', function () {
             browsers: ['last 2 versions', 'ie > 8']
         }))
         .pipe(cleanCSS())
-        .pipe(rename('iview.css'))
+        .pipe(rename('marisfrolg_iview.css'))
         .pipe(gulp.dest('../dist/styles'));
 });
 
@@ -22,4 +57,4 @@ gulp.task('fonts', function () {
         .pipe(gulp.dest('../dist/styles/fonts'));
 });
 
-gulp.task('default', ['css', 'fonts']);
+gulp.task('default', ['clean','css', 'fonts']);
--- dist/marisfrolg_iview.js
+++ dist/marisfrolg_iview.js
@@ -1551,7 +1551,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 }
 
 var API = (0, _extends3.default)({
-    version: '2.17.0',
+    version: '2.18.0',
     install: install
 }, components);
 
